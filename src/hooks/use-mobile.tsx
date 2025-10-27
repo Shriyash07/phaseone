898ad88907cc25759a7d7b22f76e3c39913c23d2
Shriyash Pawar <shriyashpawar2004@gmail.com>
2025-10-27 05:57:45 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -11,8 +11,13 @@ export function useIsMobile() {
     const checkSize = () => {
       setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
     }
+    // Set the initial value on the client
     checkSize();
+    
+    // Add event listener
     window.addEventListener("resize", checkSize)
+    
+    // Remove event listener on cleanup
     return () => window.removeEventListener("resize", checkSize)
   }, [])
 
